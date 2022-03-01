73283bde6d8a37a3b116dc30d3447b8825903cb6
crislav10 <cristianlav10@gmail.com>
2022-03-01 12:16:11 -0500

    Objetos literales y prototipos

---
+++ poo/main.js
@@ -0,0 +1,46 @@
+// Objeto Literal
+const natalia={
+    name:'Natalia',
+    age:20,
+    cursosAprobados:[
+        'Curso definitivo de HTML y CSS',
+        'Curso práctico de HTML y CSS'
+    ],
+    //dos maneras de nombrar metodos en un Objeto literal
+    aprobarCurso:function(nuevoCurso){
+        this.cursosAprobados.push(nuevoCurso);
+    },
+    cumplirA(){
+        this.age+=1;
+    }
+};
+//Hacer que Natalia apruebe otro curso
+natalia.cursosAprobados.push('Responsive Design');
+natalia.aprobarCurso('React');
+natalia.cumplirA();
+
+// Objeto con prototipo
+
+function Student(name,age,cursosAprobados){
+    this.name=name;
+    this.age=age;
+    this.cursosAprobados=cursosAprobados;
+    this.aprobarCurso=function(nuevoCurso){
+        this.cursosAprobados.push(nuevoCurso);
+    };
+}
+Student.prototype.cumplirA = function(){
+    this.age+=1;
+}
+
+const juanita= new Student(
+    'Juanita Alejandra',
+    15,
+    [
+        'Videojuegos',
+        'Creación de personajes'
+    ]
+);
+
+juanita.cumplirA();
+juanita.aprobarCurso('React');
