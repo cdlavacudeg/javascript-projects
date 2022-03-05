6dc3f984a481c1b042eb80b9d4dbea1df92dd8b0
crislav10 <cristianlav10@gmail.com>
2022-03-05 10:02:01 -0500

    Clases, generators,modules

--- ecmascript/src/es6/index.js
+++ ecmascript/src/es6/index.js
@@ -122,3 +122,38 @@ helloPromise()
     .then(response=>console.log(`response: ${response}`))
     .then(()=>console.log('Hola'))
     .catch(error=>console.log(`error: ${error}`));
+
+class calculator{
+    constructor(){
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+    sum(valueA,valueB){
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    }
+};
+
+const calc= new calculator();
+console.log(calc.sum(2,2));
+
+// Modulos
+import hello from "./module.js";
+hello();
+
+// generator
+
+function* helloWorld(){
+    if(true){
+        yield 'Hello, ';
+    }
+    if(true){
+        yield 'World';
+    }
+};
+
+const generatorHello=helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
