2b431c31c493376ab3ed6d69506da58525dad3f7
crislav10 <cristianlav10@gmail.com>
2022-03-05 17:07:26 -0500

    ES12

---
+++ ecmascript/src/es12/index.js
@@ -0,0 +1,50 @@
+// 🧯 Replace
+
+const string = "JavaScript es maravilloso, con JavaScript puedo crear el futuro de la web.";
+const replacedString = string.replace("JavaScript", "Python");
+console.log(replacedString);
+
+//// Output: Python es maravilloso, con JavaScript puedo crear el futuro de la web.
+
+const replacedString2 = string.replaceAll("JavaScript", "Python");
+console.log(replacedString2);
+
+//// Output: Python es maravilloso, con Python puedo crear el futuro de la web.
+
+// 🔒 Metodos privados
+class Message {
+    #show(val){ // Con el # convertimos al método en privado
+        console.log(val);
+    };
+    //Bloque de getters y setters
+};
+
+const message = new Message();
+message.show('Hola!');
+
+//  👽 Promise Any
+
+const promise1 = new Promise((resolve, reject) => reject("1"));
+const promise2 = new Promise((resolve, reject) => resolve("2"));
+const promise3 = new Promise((resolve, reject) => resolve("3"));
+
+Promise.any([promise1, promise2, promise3])//primero que resuelve
+    .then(response => console.log(response));
+
+
+//  🦴 WeakRef(element);
+
+class AnyClass {
+    constructor(element){
+        this.ref = new WeakRef(element);//referencia debil, evitar garbage colector
+    }
+    
+}
+
+// 🧪 Nuevos operadores lógicos
+
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue &&= isFalse);
+console.log(isTrue ||= isFalse);
+console.log(isTrue &&= isFalse);
