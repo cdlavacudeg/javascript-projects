458c408b87db0d8e52c4010c22546720580dbbd7
crislav10 <cristianlav10@gmail.com>
2022-03-09 15:27:43 -0500

    Typescript: tipos básicos de datos

--- js-pro/ejercicios/typescript/index.ts
+++ js-pro/ejercicios/typescript/index.ts
@@ -5,3 +5,47 @@ function add (a:number,b:number){
 }
 const sum=add(2,4);
 console.log(sum);
+
+// Tipos de datos
+
+// Boolean
+let muted: boolean=true;
+muted=false;
+
+// Numeros
+let age=6;
+let numerador:number = 42;
+let denominador: number= age;
+let resultado=numerador/denominador;
+
+// String
+let nombre: string='Richard';
+let saludo =`Me llamo ${nombre};`;
+
+// Arreglos
+let people: string[]=[];
+people=['Isabel','Nicol','Raul'];
+people.push('900');
+
+let peopleAndNumbers: Array<string | number>=[];
+peopleAndNumbers.push('Ricardo');
+peopleAndNumbers.push(900);
+
+// Enum -> Conjuntos de valores
+
+enum Color{
+    Rojo="Rojo",
+    Verde="Verde",
+    Azul="Azul",
+}
+
+let colorFavorito: Color=Color.Verde;
+console.log(`Mi color favorito es ${colorFavorito}`);//Mi color favorito es 1 (cuando no esta inizializado)
+
+// Any
+let comodin: any="Joker";
+comodin ={type:'Wildcar'};
+
+// Object
+let someObject: object={type:'Wildcar'}
+
