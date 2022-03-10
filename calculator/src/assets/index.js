17f667d1f879364ce04b51b063d0dd8118b87705
crislav10 <cristianlav10@gmail.com>
2022-03-10 17:14:08 -0500

    Funtional Calculator

--- calculator/src/assets/index.js
+++ calculator/src/assets/index.js
@@ -4,23 +4,47 @@ const clearButton=document.querySelector('#clear');
 const equalsButton=document.querySelector('#equals');
 
 const formulaDisplay=document.querySelector('#formula-display');
-const output=document.querySelector('#output-display');
-
+const output=document.querySelector('#display');
+let formula='';
+let done=false;
+output.innerText='0';
 numberButtons.forEach(number => {
     number.addEventListener('click',()=>{
+        if(done==true) {
+            clearVariables();
+        };
         formulaDisplay.innerText+=number.innerText;
+        if(output.innerHTML!=='0'){
+            output.innerText+=number.innerHTML;
+        }else{
+            output.innerText=number.innerHTML;
+        }
+        formula+=number.innerText;
     });
 });
 operationButtons.forEach(operation => {
     operation.addEventListener('click',()=>{
+        if(done==true) {
+            clearVariables();
+        };
         formulaDisplay.innerText+=operation.innerText;
+        formula+=operation.innerText;  
+        output.innerText='0';      
     });
 });
 clearButton.addEventListener('click',()=>{
-    formulaDisplay.innerText='';
-    output.innerText='';
+    clearVariables();
 })
 
 equalsButton.addEventListener('click',()=>{
-    output.innerText=formulaDisplay.innerText
+    console.log(formula);
+    output.innerText=eval(formula);
+    done=true;
 })
+
+clearVariables=()=>{
+    formulaDisplay.innerText='';
+    output.innerText=0;
+    formula='';
+    done=false;
+}
